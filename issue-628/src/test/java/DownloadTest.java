2900b6ccc54e2b9d57c325ee9d9633b58bc72aa7
Diego Molina <diemol@gmail.com>
2017-12-14 11:57:28 +0100

    Removing unused import.

--- issue-628/src/test/java/DownloadTest.java
+++ issue-628/src/test/java/DownloadTest.java
@@ -1,6 +1,5 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
