43390f3d7cf50489c22e1eb59eac6fe3f750a8d5
Diego Molina <diemol@gmail.com>
2019-12-05 14:50:36 +0100

    Issue 7834

---
+++ selenium/issue-7834/Issue7834/Issue7834/Issue7834Troubleshooting.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Threading;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using SeleniumExtras.WaitHelpers;
+
+namespace Issue7834
+{
+    public class Issue7834Troubleshooting
+    {
+        [Test]
+        public void Issue7834Test()
+        {
+            IWebDriver webDriver = new ChromeDriver();
+            try
+            {
+                webDriver.Navigate().GoToUrl("https://login.microsoftonline.com/");
+                var wait = new WebDriverWait(webDriver, TimeSpan.FromSeconds(10));
+                var byEmail = By.XPath("//input[@type='email']");
+                wait.Until(ExpectedConditions.ElementIsVisible(byEmail));
+                webDriver.FindElement(byEmail).SendKeys("joe@gmail.com");
+                webDriver.FindElement(By.XPath("//input[@value='Next']")).Click();
+                Thread.Sleep(20000);
+                //Switch to the alert
+                var alertHandler = webDriver.SwitchTo().Alert(); 
+                alertHandler.SetAuthenticationCredentials("joe@gmail.com", "a_fake_password"); 
+                webDriver.SwitchTo().DefaultContent();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+            finally
+            {
+                webDriver.Quit();
+            }
+        }
+        
+    }
+}
