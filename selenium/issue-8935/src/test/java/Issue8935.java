05e0b5ee3f065e0a7d8388fb8529baa88217c680
Diego Molina <diemol@gmail.com>
2020-12-05 23:20:28 +0100

    Issue 8935

---
+++ selenium/issue-8935/src/test/java/Issue8935.java
@@ -0,0 +1,18 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
+public class Issue8935 {
+  public static void main(String[] args){
+    WebDriver driver = new ChromeDriver();
+    try {
+      driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+      driver.get("https://signin.ebay.com/");
+      driver.findElement(By.xpath(".//*[@id='gh-p-1']/a")).click();
+    } finally {
+      driver.quit();
+    }
+  }
+}
