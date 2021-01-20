73241cf203a42ad688c18cf08a9f847e690c52a2
Diego Molina <diemol@gmail.com>
2021-01-20 14:05:01 +0100

    Issue 9053

--- selenium/issue-8935/src/test/java/Issue8935.java
+++ selenium/issue-8935/src/test/java/Issue8935.java
@@ -5,7 +5,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import java.time.Duration;
 
 public class Issue8935 {
-  public static void main(String[] args){
+  public static void main(String[] args) {
     WebDriver driver = new ChromeDriver();
     try {
       driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
