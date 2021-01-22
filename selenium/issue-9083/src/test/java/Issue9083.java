804dbc68787c424015416813010f0e3d4a944a1b
Diego Molina <diemol@gmail.com>
2021-01-22 11:08:57 +0100

    Issue 9083

--- selenium/issue-9083/src/test/java/Issue9083.java
+++ selenium/issue-9083/src/test/java/Issue9083.java
@@ -1,13 +1,11 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
 
 public class Issue9083 {
 
   public static void main(String[] args) {
-//    WebDriver driver = new InternetExplorerDriver();
-    WebDriver driver = new ChromeDriver();
+    WebDriver driver = new InternetExplorerDriver();
 
     try {
       driver.get("http://wssq.sbj.cnipa.gov.cn:9080/tmsve/sycjwt_getMain.xhtml");
@@ -23,6 +21,8 @@ public class Issue9083 {
         driver.findElement(By.xpath("//INPUT[@class=\"zhuceBtn\"]")).click();
         System.out.println("Clicked element successfully!");
       }
+    } catch (Exception e) {
+      System.out.println(e.getMessage());
     } finally {
       driver.quit();
     }
