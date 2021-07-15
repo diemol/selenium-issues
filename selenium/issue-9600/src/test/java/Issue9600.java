c89dc43515491ebb2dce049863a671ab8260f9da
Diego Molina <diemol@gmail.com>
2021-07-15 15:42:00 +0200

    Issue 9600

--- selenium/issue-9600/src/test/java/Issue9600.java
+++ selenium/issue-9600/src/test/java/Issue9600.java
@@ -1,5 +1,4 @@
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.ie.InternetExplorerOptions;
 import org.openqa.selenium.remote.RemoteWebDriver;
 
@@ -9,9 +8,8 @@ import java.net.URL;
 public class Issue9600 {
 
   public static void main(String[] args) throws MalformedURLException, InterruptedException {
-//    InternetExplorerOptions ieOptions = new InternetExplorerOptions();
-//    WebDriver driver = new RemoteWebDriver(new URL("http://localhost:4444"), ieOptions);
-    InternetExplorerDriver driver = new InternetExplorerDriver();
+    InternetExplorerOptions ieOptions = new InternetExplorerOptions();
+    WebDriver driver = new RemoteWebDriver(new URL("http://localhost:4444"), ieOptions);
     driver.get("http://www.google.com");
     Thread.sleep(10000);
     driver.quit();
