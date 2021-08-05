c4351d80f8fe847f4f50d8187de2f1201d2b4acd
Diego Molina <diemol@gmail.com>
2021-08-05 22:30:19 +0200

    Issue 1325

---
+++ docker-selenium/issue-1325/SeleniumDynamicissueTest/SeleniumDynamicissueTest/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Remote;
+
+namespace SeleniumDynamicissueTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var driverOptions = new FirefoxOptions();
+
+            driverOptions.AddAdditionalOption("se:recordVideo", true);
+            driverOptions.AddAdditionalOption("se:timeZone", "US/Pacific");
+            driverOptions.AddAdditionalOption("se:screenResolution", "1920x1080");
+
+            var driver = new RemoteWebDriver(new Uri("http://localhost:4444"), driverOptions);
+            driver.Navigate().GoToUrl("http://www.google.com");
+            Thread.Sleep(5000);
+            driver.Quit();
+ 
+        }
+    }
+}
