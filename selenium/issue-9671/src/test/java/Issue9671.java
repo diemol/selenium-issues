4378dbcfb95ec214fe7c412efc5ddeed9e3a6589
Diego Molina <diemol@gmail.com>
2021-12-13 16:59:43 +0100

    Issue 9671

--- selenium/issue-9671/src/test/java/Issue9671.java
+++ selenium/issue-9671/src/test/java/Issue9671.java
@@ -1,17 +1,24 @@
+import org.openqa.selenium.OutputType;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.devtools.DevTools;
-import org.openqa.selenium.devtools.v96.network.Network;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
-import java.util.Optional;
+import java.util.logging.Handler;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class Issue9671 {
 
   public static void main (String[] args) {
+    Logger logger = Logger.getLogger(RemoteWebDriver.class.getName());
+    logger.setLevel(Level.FINER);
+
+    for (Handler handler : Logger.getLogger("").getHandlers()) {
+      handler.setLevel(Level.FINER);
+    }
     ChromeDriver driver = new ChromeDriver();
-    DevTools devTools = driver.getDevTools();
-    devTools.createSession();
-    devTools.send(Network.enable(Optional.empty(), Optional.empty(), Optional.empty()));
+
     driver.navigate().to("https://google.com");
+    driver.getScreenshotAs(OutputType.BYTES);
     driver.quit();
   }
 
