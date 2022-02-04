f0c67123522262e8f281b7bc3de6cf2034154405
Diego Molina <diemol@gmail.com>
2022-02-04 12:28:27 +0100

    Adding code for issue 1500 in docker selenium

---
+++ docker-selenium/issue-1500/src/test/java/Issue1500.java
@@ -0,0 +1,18 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class Issue1500 {
+
+  public static void main (String[] args) throws MalformedURLException {
+    ChromeOptions options = new ChromeOptions();
+    options.setHeadless(true);
+    WebDriver driver = new RemoteWebDriver(new URL("http://localhost:4444"), options);
+    driver.get("https://selenium.dev");
+    driver.quit();
+  }
+
+}
