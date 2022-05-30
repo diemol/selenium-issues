57e132c6fc4ec8b25a9b94487176f95a959a99c2
Diego Molina <diemol@gmail.com>
2022-05-30 15:10:36 +0200

    Issue 10711

---
+++ selenium/issue-10711/src/test/java/Issue10711.java
@@ -0,0 +1,46 @@
+import org.openqa.selenium.PageLoadStrategy;
+import org.openqa.selenium.Platform;
+import org.openqa.selenium.UnexpectedAlertBehaviour;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.remote.CapabilityType;
+
+import java.time.Duration;
+
+public class Issue10711 {
+
+  public static void main (String[] args) {
+    WebDriver driver = initializeFirefox();
+    driver.navigate().to("https://duckduckgo.com/");
+    driver.quit();
+  }
+
+  static ChromeDriver initializeChrome(){
+    ChromeOptions options = new ChromeOptions();
+    options.setCapability(CapabilityType.PLATFORM_NAME, Platform.ANY);
+    options.setHeadless(false);
+    options.addArguments("--start-maximized");
+    options.setUnhandledPromptBehaviour(UnexpectedAlertBehaviour.ACCEPT_AND_NOTIFY);
+    options.setCapability(CapabilityType.ACCEPT_INSECURE_CERTS, true);
+    options.setPageLoadStrategy(PageLoadStrategy.NORMAL); // https://www.skptricks.com/2018/08/timed-out-receiving-message-from-renderer-selenium.html
+    options.setPageLoadTimeout(Duration.ofSeconds(30000));
+    options.setScriptTimeout(Duration.ofSeconds(30000));
+    return new ChromeDriver(options);
+  }
+
+  static FirefoxDriver initializeFirefox(){
+    FirefoxOptions options = new FirefoxOptions();
+    options.setCapability(CapabilityType.PLATFORM_NAME, Platform.ANY);
+    options.setHeadless(false);
+    options.addArguments("--start-maximized");
+    options.setUnhandledPromptBehaviour(UnexpectedAlertBehaviour.ACCEPT_AND_NOTIFY);
+    options.setCapability(CapabilityType.ACCEPT_INSECURE_CERTS, true);
+    options.setPageLoadStrategy(PageLoadStrategy.NORMAL); // https://www.skptricks.com/2018/08/timed-out-receiving-message-from-renderer-selenium.html
+    options.setPageLoadTimeout(Duration.ofSeconds(30000));
+    options.setScriptTimeout(Duration.ofSeconds(30000));
+    return new FirefoxDriver(options);
+  }
+}
