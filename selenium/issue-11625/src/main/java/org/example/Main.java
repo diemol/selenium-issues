98bff8423daadb574628936b55bd8d2006ebcf9f
Diego Molina <diemol@gmail.com>
2023-03-08 14:41:56 +0100

    Removing unneeded caps

--- selenium/issue-11625/src/main/java/org/example/Main.java
+++ selenium/issue-11625/src/main/java/org/example/Main.java
@@ -9,26 +9,22 @@ import java.net.URL;
 
 public class Main {
 
-  public static void main(String[] args) {
-    System.out.println("Hello world!");
+  public static void main(String[] args) throws MalformedURLException {
+    matchNode2();
   }
 
   private static void matchNode1() throws MalformedURLException {
     InternetExplorerOptions options = new InternetExplorerOptions();
-    options.setCapability("ie.edgechromium", true);
-    options.withEdgeExecutablePath("C:\\Program Files (x86)\\Microsoft\\Edge\\Application\\msedge.exe");
     options.setCapability("c:custom-cap", false);
-    WebDriver driver = new RemoteWebDriver(new URL("http://hubIPaddress:4444"), options);
+    WebDriver driver = new RemoteWebDriver(new URL("http://localhost:4444"), options);
     driver.get("https://selenium.dev");
     driver.quit();
   }
 
   private static void matchNode2() throws MalformedURLException {
     InternetExplorerOptions options = new InternetExplorerOptions();
-    options.setCapability("ie.edgechromium", true);
-    options.withEdgeExecutablePath("C:\\Program Files (x86)\\Microsoft\\Edge\\Application\\msedge.exe");
     options.setCapability("c:custom-cap", true);
-    WebDriver driver = new RemoteWebDriver(new URL("http://hubIPaddress:4444"), options);
+    WebDriver driver = new RemoteWebDriver(new URL("http://localhost:4444"), options);
     driver.get("https://selenium.dev");
     driver.quit();
   }
